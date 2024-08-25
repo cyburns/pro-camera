84f57abb2516a81e809cc8aeef7113779585002a
cyburns <72774499+cyburns@users.noreply.github.com>
2024-08-25 18:59:37 -0400

    perms

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,25 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/components/useColorScheme';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Redirect, Stack, useRouter } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import { useColorScheme } from "@/components/useColorScheme";
+import { Camera, useCameraPermission } from "react-native-vision-camera";
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: "(tabs)",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -23,7 +27,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
   });
 
@@ -46,13 +50,34 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
+  const { hasPermission } = useCameraPermission();
+  const microphonePermissionStatus = Camera.getMicrophonePermissionStatus();
+
+  const hasPermissions =
+    hasPermission && microphonePermissionStatus === "granted";
+
   const colorScheme = useColorScheme();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!hasPermissions) {
+      //@ts-ignore
+      router.replace("permissions");
+    } else {
+      //@ts-ignore
+      router.replace("(tabs)");
+    }
+  }, [hasPermissions]);
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+        <Stack.Screen
+          name="permissions"
+          options={{ presentation: "modal", headerShown: true }}
+        />
       </Stack>
     </ThemeProvider>
   );
